1a803f6bfe8a08381b9ab470505b5e9c72b0224c
kiyoto1022 <kiyoto1022@gmail.com>
2017-07-04 23:56:30 +0900

    32bitでも16進数変換ができるように修正

--- ethernet_address_converter.c
+++ ethernet_address_converter.c
@@ -13,49 +13,43 @@ static long int convert_str_to_hexadecimal(const char hex_code) {
 	return -1;
 }
 
-static bool is_hex_code(const char hex_code) {
-	if (convert_str_to_hexadecimal(hex_code) != -1)
-		return true;
-	return false;
+static int get_hexadecimal(const char previous_4_bits, char after_4_bits) {
+	return convert_str_to_hexadecimal(previous_4_bits) * 16 + convert_str_to_hexadecimal(after_4_bits); 
 }
 
-static bool is_shift(int now, int loop_count) {
-	if (now + 1 < loop_count)
-		return true;
-	return false;
-}
-
-static int byte_order(long int hexadecimal) {
+static u_char *byte_order(const u_char *hexadecimal) {
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-    printf("Byte order is Little endian\n");
-    long int little_endian = 0;
-    while(1) {
-	    little_endian += hexadecimal >> 8 & 0xFF;
-	    hexadecimal = hexadecimal >> 8;
-	    if (hexadecimal <= 0)
-	    	break;
-	    little_endian = little_endian << 8;
-    }
-    return little_endian;
+	static u_char little_endian[ETHERNET_ADDRESS_LENGTH];
+	int i, j;
+	for (i=5, j=0; i>=0; i--, j++) {
+		little_endian[j] = hexadecimal[i];
+	}
+
+    printf("Byte order is Little endian [%x:%x:%x:%x:%x:%x] \n",
+    		little_endian[0], little_endian[1], little_endian[2],
+    		little_endian[3], little_endian[4], little_endian[5]);
+
+	return little_endian;
+
 #elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-    printf("Byte order is Big endian¥n");
-    return hexadecimal;
+    printf("Byte order is Big endian [%x:%x:%x:%x:%x:%x] \n",
+    		hexadecimal[0], hexadecimal[1], hexadecimal[2],
+    		hexadecimal[3], hexadecimal[4], hexadecimal[5]);
+
+	return hexadecimal;
+
 #endif
 }
 
-int convert_ethernet_address_to_hexadecimal(const char *ethernet_address) {
-    long int hexadecimal = 0;
-    int loop_count = strlen(ethernet_address);
-    for(int i=0; i<loop_count; i++) {	
-
-    	if (is_hex_code(ethernet_address[i]) == false)
-    		continue; // skip colon
-
-    	hexadecimal += convert_str_to_hexadecimal(ethernet_address[i]);
-
-    	if (is_shift(i, loop_count))
-    		hexadecimal = hexadecimal << 4;
+u_char *convert_ethernet_address_to_hexadecimal(const char *ethernet_address) {
+    u_char hexadecimal[ETHERNET_ADDRESS_LENGTH];
+    for(int i=0; i<ETHERNET_ADDRESS_LENGTH; i++) {
+    	hexadecimal[i] = (u_char)get_hexadecimal(ethernet_address[i*3], ethernet_address[i*3+1]); 
     }
-    printf("Convert Ethernet address %s to %lx\n", ethernet_address, hexadecimal);
+
+    printf("Convert to %x:%x:%x:%x:%x:%x\n",
+    		hexadecimal[0], hexadecimal[1], hexadecimal[2],
+    		hexadecimal[3], hexadecimal[4], hexadecimal[5]);
+
     return byte_order(hexadecimal);
 }
--- ethernet_address_converter.h
+++ ethernet_address_converter.h
@@ -1,1 +1,3 @@
-int convert_ethernet_address_to_hexadecimal(const char *ethernet_address);
+#define ETHERNET_ADDRESS_LENGTH 6
+
+u_char *convert_ethernet_address_to_hexadecimal(const char *ethernet_address);
