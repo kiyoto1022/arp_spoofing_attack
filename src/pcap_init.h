52c723e7679806261d817774379733eb488c5de9
kiyoto1022 <kiyoto1022@gmail.com>
2017-07-29 23:14:47 +0900

    pcap_init.hのdefineの記述を修正する

--- src/pcap_init.h
+++ src/pcap_init.h
@@ -1,8 +1,8 @@
 #include <pcap/pcap.h>
 
 #define ETHERNET_FRAME_SIZE 1518
-#define PROMISCUOUS_MODE_ON = 1
-#define TIME_OUT = 1000
-#define OPTIMISATION_OFF = 0
+#define PROMISCUOUS_MODE_ON 1
+#define TIME_OUT 1000
+#define OPTIMISATION_OFF 0
 
 pcap_t *init_pcap(char *interface_name);
