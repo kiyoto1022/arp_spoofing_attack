a0b93493b7e400fe2418955bd2c042252db2246e
kiyoto1022 <kiyoto1022@gmail.com>
2017-08-08 00:52:06 +0900

    先祖返りしたpcapの初期化関数を戻す

--- src/pcap_init.c
+++ src/pcap_init.c
@@ -3,49 +3,7 @@
 #include <pcap/pcap.h>
 #include "pcap_init.h"
 
-static pcap_t *open_capture_device(char *interface_name) {
-
-	static pcap_t *handle;
-	char errbuf[PCAP_ERRBUF_SIZE];
-
-	handle = pcap_open_live(interface_name, ETHERNET_FRAME_SIZE, 1, 1000, errbuf);
-	if (handle == NULL) {
-		printf("Couldn't open device %s: %s\n", interface_name, errbuf);
-		exit(1);
-	}
-	return handle;
-}
-
-static void is_link_layer_ethernet(pcap_t *handle, char *interface_name) {
-
-	char errbuf[PCAP_ERRBUF_SIZE];
-
-	if (pcap_datalink(handle) != DLT_EN10MB) {
-		printf("%s is not an Ethernet\n", interface_name);
-		exit(1);
-	}
-}
-
-static void compile_the_filter(pcap_t *handle, bpf_u_int32 network_address) {
-
-	char filter_exp[] = "ip";
-	struct bpf_program fp;
-
-	if (pcap_compile(handle, &fp, filter_exp, 0, network_address) == -1) {
-		printf("Can't compile pcap filter %s: %s\n", filter_exp, pcap_geterr(handle));
-		exit(1);
-	}
-
-	if (pcap_setfilter(handle, &fp) == -1) {
-		printf("Coundn't install filter %s:%s\n", filter_exp, pcap_geterr(handle));
-		exit(1);
-	}
-
-	pcap_freecode(&fp);
-}
-
-
-static void pcap_lookup(char * interface_name, bpf_u_int32 *network_address, bpf_u_int32 *subnet_mask) {
+static void get_interface_info(char * interface_name, bpf_u_int32 *network_address, bpf_u_int32 *subnet_mask) {
 
 	char errbuf[PCAP_ERRBUF_SIZE];
 
@@ -56,6 +14,47 @@ static void pcap_lookup(char * interface_name, bpf_u_int32 *network_address, bpf
 	}
 }
 
+static pcap_t *open_capture_device(char *interface_name) {
+
+	static pcap_t *handle;
+	char errbuf[PCAP_ERRBUF_SIZE];
+
+	handle = pcap_open_live(interface_name, ETHERNET_FRAME_SIZE, PROMISCUOUS_MODE_ON, TIME_OUT, errbuf);
+	if (handle == NULL) {
+		printf("Couldn't open device %s: %s\n", interface_name, errbuf);
+		exit(1);
+	}
+	return handle;
+}
+
+static void is_link_layer_ethernet(pcap_t *handle) {
+
+	char errbuf[PCAP_ERRBUF_SIZE];
+
+	if (pcap_datalink(handle) != DLT_EN10MB) {
+		printf("Not an Ethernet\n");
+		exit(1);
+	}
+}
+
+static void compile_the_filter(pcap_t *handle, bpf_u_int32 network_address) {
+
+	char filter[] = "ip";
+	struct bpf_program compile_result;
+
+	if (pcap_compile(handle, &compile_result, filter, OPTIMISATION_OFF, network_address) == -1) {
+		printf("Can't compile pcap filter %s: %s\n", filter, pcap_geterr(handle));
+		exit(1);
+	}
+
+	if (pcap_setfilter(handle, &compile_result) == -1) {
+		printf("Coundn't install filter %s: %s\n", filter, pcap_geterr(handle));
+		exit(1);
+	}
+
+	pcap_freecode(&compile_result);
+}
+
 pcap_t *init_pcap(char *interface_name) {
 
 	static pcap_t *handle;
@@ -63,11 +62,11 @@ pcap_t *init_pcap(char *interface_name) {
 	bpf_u_int32 network_address;
 	bpf_u_int32 subnet_mask;
 
-	pcap_lookup(interface_name, &network_address, &subnet_mask);
+	get_interface_info(interface_name, &network_address, &subnet_mask);
 
 	handle = open_capture_device(interface_name);
 
-	is_link_layer_ethernet(handle, interface_name);
+	is_link_layer_ethernet(handle);
 	compile_the_filter(handle, network_address);
 
 	return handle;
