b51580e9eeffb974c1bf5b78f05ce20ea6e10119
kiyoto1022 <kiyoto1022@gmail.com>
2017-08-10 23:01:30 +0900

    パケットの書き換えをするため別の変数にコピーする

--- src/packet_forward.c
+++ src/packet_forward.c
@@ -10,8 +10,14 @@
 
 int count_tcp = 0;
 int count_udp = 0;
+int count_other = 0;
 
-static void change_ethernet_address(const u_char *packet, struct spoofing_attack *spoofing_attack) {
+static int get_packet_size(const u_char *packet) {
+	struct ip_header *ip_hdr = (struct ip_header*)(packet + 14);
+	return ntohs(ip_hdr->total_length);
+}
+
+static void change_ethernet_address(u_char *packet, struct spoofing_attack *spoofing_attack) {
 
 	struct ethernet_header *eth_header = (struct ethernet_header*)packet;
 	
@@ -26,13 +32,12 @@ static void change_ethernet_address(const u_char *packet, struct spoofing_attack
 	}
 }
 
-static void forwarding(const u_char *packet, struct spoofing_attack *spoofing_attack) {
+static void forwarding(u_char *packet, struct spoofing_attack *spoofing_attack) {
 	
 	struct sockaddr sockaddr;
 	strncpy(sockaddr.sa_data, spoofing_attack->interface_name, sizeof(sockaddr.sa_data));
 
-	// TODO: Set Packet size.
-	if(sendto(spoofing_attack->send_socket, packet, 1500, 0, &sockaddr, sizeof(sockaddr)) < 0) {
+	if(sendto(spoofing_attack->send_socket, packet, get_packet_size(packet), 0, &sockaddr, sizeof(sockaddr)) < 0) {
 		printf("\nFailed to send forwarding : %s\n", strerror(errno));
 		return;
 	}
@@ -44,8 +49,9 @@ static void packet_count(const u_char *packet) {
 	switch(ip_hdr->protocol) {
 		case IPPROTO_TCP: count_tcp += 1; break;
 		case IPPROTO_UDP: count_udp += 1; break;
+		default: count_other += 1; break;
 	}
-	printf("\rTCP: %d Packet , UDP: %d Packet", count_tcp, count_udp);
+	printf("\rTCP: %d Packet , UDP: %d Packet , OTHER: %d Packet", count_tcp, count_udp, count_other);
 }
 
 void forward(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
@@ -54,9 +60,12 @@ void forward(u_char *args, const struct pcap_pkthdr *header, const u_char *packe
 
 	packet_count(packet);
 
-	change_ethernet_address(packet, spoofing_attack);
+	u_char fowarding_packet[get_packet_size(packet)];
+	memcpy(fowarding_packet, packet, get_packet_size(packet));
 
-	forwarding(packet, spoofing_attack);
+	change_ethernet_address(fowarding_packet, spoofing_attack);
+
+	forwarding(fowarding_packet, spoofing_attack);
 
 	// To avoid No buffer space available Error.
 	usleep(100);
